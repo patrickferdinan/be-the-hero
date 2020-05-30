24d715707f8ef8576257a805af0dfcab33760761
Patrick Ferdinan <patrick.ferdinan@hotmail.com>
2020-05-30 15:17:07 -0300

    semana omnistack 11.0

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// JSX (Javascript XML)
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+
